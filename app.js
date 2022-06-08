cbb9ceaab865ca6138f21f843636ffedf85ae668
Javier <Javier19pedraza@gmail.com>
2022-06-08 14:40:37 -0500

    wac a mole game

---
+++ app.js
@@ -0,0 +1,48 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const score =  document.querySelector('#score')
+const timeLeft =  document.querySelector('#time-left')
+
+
+let result = 0 
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+function randomSquare(){
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+    hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPosition){
+            result++ 
+            console.log(result)
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole(){
+    timerId = setInterval(randomSquare,1000)
+}
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+    if(currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('Game Over your score is: ' + result )
+    }   
+}
+moveMole()
+
+let countDownTimerId = setInterval(countDown, 1000)
